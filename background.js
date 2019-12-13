c462ad6cf53fedf5955af60a66fcbc3e345a99c1
Sarra Souissi <sarrasouissi@Sarras-MacBook-Pro-4.local>
2019-12-12 19:48:34 -0500

    changed setup for js script

--- background.js
+++ background.js
@@ -1,22 +1,27 @@
-function createWindow () {
-    chrome.windows.create(
-      {
-        url: chrome.extension.getURL("uwuify.html"),
-        type: "panel",
-        focused: true,
-        top: 60,
-        left: 20,
-        width: 200,
-        height: 200
-      },
-      window => {
-        window.WindowType = "app";
-        window.alwaysOnTop = true;
-      }
-    );
-  }
+// function createWindow () {
+//     chrome.windows.create(
+//       {
+//         url: chrome.extension.getURL("uwuify.html"),
+//         type: "panel",
+//         focused: true,
+//         top: 60,
+//         left: 20,
+//         width: 200,
+//         height: 200
+//       },
+//       window => {
+//         window.WindowType = "app";
+//         window.alwaysOnTop = true;
+//       }
+//     );
+//   }
   
-  // Called when the user clicks on the browser action.
-  chrome.browserAction.onClicked.addListener((tab) => {
-    createWindow(); 
-  });
+//   // Called when the user clicks on the browser action.
+//   chrome.browserAction.onClicked.addListener((tab) => {
+//     alert("test alert")
+//     createWindow(); 
+//   });
+
+chrome.browserAction.onClicked.addListener(function(tab) {
+    chrome.tabs.executeScript(null, {file: "uwuify.js"});
+ });
--- content.js
+++
@@ -1,9 +0,0 @@
-// Get the highlighted text on the page and return it directly to the message sender
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if (request.action === "getText") {
-      let highlightedText = window.getSelection().toString();
-      sendResponse({
-        text: highlightedText
-      });
-    }
-  });
